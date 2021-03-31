3344be148a64199a6c6acb1532a9a283025f8077
mingjitianming <37174134+mingjitianming@users.noreply.github.com>
2021-03-31 19:47:49 +0800

    Update Singleton.cpp

--- Singleton.cpp
+++ Singleton.cpp
@@ -8,15 +8,9 @@ private:
 public:
 	static Singleton* getSingleton()
 	{
-		if (instance_)
-		{
-			return instance_;
-		}
-		else
-		{
-			instance_ = new Singleton();
-			return instance_;
-		}
+		static std::shared_ptr<Singleton> instance = std::make_shared<Singleton>();
+		return instance;
+		
 	}
 
 	~Singleton()
@@ -26,23 +20,18 @@ public:
 
 	void printAdd()
 	{
-		printf("address is %d\n", instance_);
+		printf("address is %d\n", instance);
 	}
 
-public:
-	static Singleton* instance_;
 };
 
-Singleton* Singleton::instance_ = NULL;
 
 int main ()
 {
-	Singleton* test1 = Singleton::getSingleton();
-	Singleton* test2 = Singleton::getSingleton();
+	auto test1 = Singleton::getSingleton();
+	auto test2 = Singleton::getSingleton();
 	test1->printAdd();
 	test2->printAdd();
-
-	if (Singleton::instance_) delete Singleton::instance_;
 	
 	return 0;
 }
