4da125e546b31b4e7f882016a4792b1c3d02104a
chennuo <chennuo0125@163.com>
2021-04-01 20:38:24 +0800

    add FindPartiallySortedMatrix

---
+++ FindPartiallySortedMatrix.cpp
@@ -0,0 +1,49 @@
+#include <cstdio>
+
+bool Find( int **matrix, int rows, int cols, int number)
+{
+	if (matrix == NULL) return false;
+	
+	int rux = cols - 1; // 右上角x索引
+	int ruy = 0; // 右上角y索引
+	while (rux >= 0 && ruy < rows)
+	{
+		if (number == matrix[rux][ruy]) return true;
+
+		// 如果数组元素大于检索值，则该列数值都大于检索值，可以跳过这一列
+		if (number < matrix[rux][ruy])
+		{
+			rux -= 1;
+			continue;
+		}
+		// 如果数组元素小于检索值，则该行数值都小于检索值，可以跳过这一行
+		if (number > matrix[rux][ruy])
+		{
+			ruy += 1;
+			continue;
+		}
+	}
+	
+	return false;
+}
+
+int main ()
+{
+	const int rows = 4;
+	const int cols = 4;
+	const int number = 7;
+
+	int matrix_arr[rows][cols] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+
+	int **matrix = new int*[cols];
+
+	for (int i = 0; i < rows; i++)
+		matrix[i] = matrix_arr[i];
+	
+	if (Find(matrix, rows, cols, number)) printf("search success\n");
+	else printf("search fail !!!\n");
+
+	delete[] matrix;
+
+	return 0;
+}
