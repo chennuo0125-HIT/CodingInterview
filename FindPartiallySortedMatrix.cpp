1103ce95caeafbfd592e25ea9d6ff64fa5e343bc
chennuo <chennuo0125@163.com>
2021-04-01 20:55:04 +0800

    temp

--- FindPartiallySortedMatrix.cpp
+++ FindPartiallySortedMatrix.cpp
@@ -35,7 +35,7 @@ int main ()
 
 	int matrix_arr[rows][cols] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
 
-	int **matrix = new int*[cols];
+	int **matrix = new int*[rows];
 
 	for (int i = 0; i < rows; i++)
 		matrix[i] = matrix_arr[i];
